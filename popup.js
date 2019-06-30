0fd03de8955f3fe1156c75db97a28b41d64e78ff
Tayg0 <36306875+Tayg0@users.noreply.github.com>
2019-06-30 13:49:36 -0400

    Implemented functions to load stream info. Basic placeholder styling.
    Loads and populates streamer names and stream titles.

--- popup.js
+++ popup.js
@@ -5,32 +5,104 @@ var client_id = "va97w97mn1qzq0nlrjavlifr92lstz";
 
 $(document).ready(function(){
 
+    loginToID(user_login);
 
-    login2id(user_login);
-
-    // var URL = "https://api.twitch.tv/kraken/users/" + encodeURI(user) + "/follows/channels?limit=100&client_id=" + client_id;
-    // URL = "https://api.twitch.tv/kraken/users?login=TayG0&client_id=va97w97mn1qzq0nlrjavlifr92lstz";
-
-    $(".header").append("Testing!")
 })
 
-function login2id(login){
+function loginToID(login){
 
     user_id = "";
 
     $.ajax({
         type: 'GET',
-        url: 'https://api.twitch.tv/helix/users?login=TayG0',
+        url: 'https://api.twitch.tv/helix/users?login=' + login,
         dataType: 'json',
         headers:{
-          'Client-ID': 'va97w97mn1qzq0nlrjavlifr92lstz'
+          'Client-ID': client_id
         },
         success: function(data) {
           console.log("Login: " + data.data[0].login + "   ID: " + data.data[0].id);
           user_id = data.data[0].id;
+          getFollows(user_id);
         }
        });
 
 
 }
 
+function getFollows(id){
+
+    $.ajax({
+        type: 'GET',
+        url: 'https://api.twitch.tv/helix/users/follows?first=100&from_id=' + id,
+        dataType: 'json',
+        headers:{
+          'Client-ID': client_id
+        },
+        success: function(data) {
+
+            var add = "";
+
+            $.each(data.data, function(index, value){
+                add += "&user_login=" + value.to_name;
+            });
+
+            console.log(data);
+            console.log(add);
+            getStreams(add);
+
+        }
+       });
+
+}
+
+function getStreams(addon){
+
+    $.ajax({
+        type: 'GET',
+        url: 'https://api.twitch.tv/helix/streams?first=100' + addon,
+        dataType: 'json',
+        headers:{
+          'Client-ID': client_id
+        },
+        success: function(data) {
+          console.log(data);
+          printStreams(data);
+        }
+       });
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+function printStreams(data){
+
+    $.each(data.data, function(index, value){
+
+        $("table").append(
+            "<tr id=\""+index+"\">"+
+            "<td>"+
+            value.user_name+
+            "</td>"+
+            "<td>"+
+            value.title+
+            "</td>"+
+            "</tr>"
+        );
+        
+        $("#" + index).click(function(){$(document).location = "https://www.twitch.tv/" + value.user_name})
+    });
+    console.log("Done.");
+    chrome.browserAction.setBadgeText({text: data.data.length + ""})
+    chrome.browserAction.setBadgeBackgroundColor({color: "#9800ff"})
+}
