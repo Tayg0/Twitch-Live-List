2ae863e2df0e530fdda16b0f175feed195aa5aa9
Tayg0 <36306875+Tayg0@users.noreply.github.com>
2019-06-30 11:02:53 -0400

    Implement function to convert user login to user id using Twitch API.

--- popup.js
+++ popup.js
@@ -1,9 +1,36 @@
+
+var user_login = "TayG0";
+var user_id = "";
+var client_id = "va97w97mn1qzq0nlrjavlifr92lstz";
+
 $(document).ready(function(){
 
-$(".header").append("Testing")
-    
 
+    login2id(user_login);
 
+    // var URL = "https://api.twitch.tv/kraken/users/" + encodeURI(user) + "/follows/channels?limit=100&client_id=" + client_id;
+    // URL = "https://api.twitch.tv/kraken/users?login=TayG0&client_id=va97w97mn1qzq0nlrjavlifr92lstz";
 
-
+    $(".header").append("Testing!")
 })
+
+function login2id(login){
+
+    user_id = "";
+
+    $.ajax({
+        type: 'GET',
+        url: 'https://api.twitch.tv/helix/users?login=TayG0',
+        dataType: 'json',
+        headers:{
+          'Client-ID': 'va97w97mn1qzq0nlrjavlifr92lstz'
+        },
+        success: function(data) {
+          console.log("Login: " + data.data[0].login + "   ID: " + data.data[0].id);
+          user_id = data.data[0].id;
+        }
+       });
+
+
+}
+
