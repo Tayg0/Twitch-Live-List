19be033a321cad8c6764813a43387f5dcf58b5bf
Tayg0 <36306875+Tayg0@users.noreply.github.com>
2019-07-01 16:47:25 -0400

    Changed to singlewide stream list. CSS polishing. List entries link to twitch pages.
    Base functionality complete. Todo: Sort by Game. Headers and branding. Code cleanup and formatting.

--- js/popup.js
+++ js/popup.js
@@ -3,19 +3,10 @@ var user_login = "TayG0";
 var user_id = "";
 var client_id = "va97w97mn1qzq0nlrjavlifr92lstz";
 
-var templateCard = "<div class=\"col-xs-3\"><div class=\"card\" style=\"width:128px\"><img class=\"card-img-top\" src=\"{THUMBURL}\" alt=\"Card image\"><div class=\"card-img-overlay\"><p class=\"card-text\">🙍‍{VIEWS}</p></div><div class=\"card-body\"><h6 class=\"card-title\">{USERNAME}</h6><p class=\"card-text\">{GAME}</p></div></div></div>";
+var templateCard = "<div class=\"col-xs-3\"><div class=\"card\" style=\"width:291px\"><div class=\"card-body\"><h6 class=\"card-title\">{USERNAME}</h6><p class=\"card-text\">{TITLE}</p></div></div></div>";
+var templateThumbCard = "<div class=\"col-xs-3\"><div class=\"card\" style=\"width:80px\"><img class=\"card-img\" src=\"{THUMBURL}\" alt=\"Stream Thumb\"></div></div>";
 
 
-
-
-
-
-
-
-
-
-
-;
 
 
 
@@ -104,8 +95,14 @@ function getStreams(addon){
 
 function printStreams(data){
 
+
+
+
     $.each(data.data, function(index, value){
-      $(".row").append(templateCard.replace("{USERNAME}", value.user_name).replace("{THUMBURL}", value.thumbnail_url).replace("{width}x{height}", "128x72").replace("{VIEWS}", "" + value.viewer_count))
+      $("#stream-list").append("<div class=\"row\" id=\"stream-"+value.user_id+"\"></div>");
+      $("#stream-"+value.user_id).append(templateThumbCard.replace("{THUMBURL}", value.thumbnail_url).replace("{width}x{height}", "80x45"));
+      $("#stream-"+value.user_id).append(templateCard.replace("{USERNAME}", value.user_name).replace("{TITLE}", value.title));
+      $("#stream-"+value.user_id).click(function(){chrome.tabs.create({url: "https://www.twitch.tv/"+encodeURI(value.user_name)})})
       console.log("Done.");
     });
     console.log("Done.");
