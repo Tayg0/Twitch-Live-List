57827464bdcde587e0c9c25c1eb794855941a89d
Tayg0 <36306875+Tayg0@users.noreply.github.com>
2019-07-04 12:02:14 -0400

    Added viewcount to stream info.

--- js/popup.js
+++ js/popup.js
@@ -1,5 +1,5 @@
 var templateGameHeaderCard = "<div class=\"row\" id=\"{GAME-ID}\"><div class=\"col-xs-12 col-game-head\"><div class=\"card game-head\"><div class=\"card-body card-body-game-head\">{GAME}</div></div></div></div>";
-var templateCard = "<div class=\"col-xs-3 col-stream-info\"><div class=\"card stream-info\"><div class=\"card-body card-body-stream-info\"><h6 class=\"card-title\">{USERNAME}<span class=\"viewers-text\">●387</span></h6><p class=\"card-text\">{TITLE}</p></div></div></div>";
+var templateCard = "<div class=\"col-xs-3 col-stream-info\"><div class=\"card stream-info\"><div class=\"card-body card-body-stream-info\"><h6 class=\"card-title\">{USERNAME}<span class=\"viewers-text\">👥{VIEWERS}</span></h6><p class=\"card-text\">{TITLE}</p></div></div></div>";
 var templateThumbCard = "<div class=\"col-xs-3 col-stream-thumb\"><div class=\"card stream-thumb\" style=\"\"><img class=\"card-img\" src=\"{THUMBURL}\" alt=\"Stream Thumb\"></div></div>";
 var game_ids = JSON.parse(localStorage.game_ids);
 
@@ -23,7 +23,7 @@ function printStreams(data) {
         $("#"+value.game_id).after("<div class=\"row row-stream\" id=\"stream-" + value.user_id + "\"></div>");
         $("#stream-" + value.user_id).append(templateThumbCard.replace("{THUMBURL}", value.thumbnail_url).replace("{width}x{height}", "320x180"));
         //$("#stream-"+value.user_id).append(templateThumbCard.replace("{THUMBURL}", value.thumbnail_url).replace("{width}x{height}", "80x45"320x180));
-        $("#stream-" + value.user_id).append(templateCard.replace("{USERNAME}", value.user_name).replace("{TITLE}", value.title));
+        $("#stream-" + value.user_id).append(templateCard.replace("{USERNAME}", value.user_name).replace("{TITLE}", value.title).replace("{VIEWERS}", value.viewer_count));
         $("#stream-" + value.user_id).click(function () { chrome.tabs.create({ url: "https://www.twitch.tv/" + encodeURI(value.user_name) }) })
         console.log("Done.");
     });
@@ -31,3 +31,9 @@ function printStreams(data) {
     console.log(localStorage.Test);
 }
 
+// function onStorageEvent(storageEvent){
+
+//     alert("storage event");
+// }
+
+// window.addEventListener('storage', onStorageEvent, false);
